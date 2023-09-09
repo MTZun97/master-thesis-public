93775920dd27a34470dadfadf59a79cb00d05843
Moe Thiri Zun <moethiri.zun97@gmail.com>
2023-09-09 20:21:59 +0900

    delete extra files - test2.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from manufacturer import manufacturer_count
 from projects import project_count
 from project_choropleth import generate_choropleth
 from electrolyzer_cost_reduction import plot_cost_reduction  # Corrected missing import statement
+from global_lcoh import capacity_factor, global_lcoh
+
 
 # Define custom color codes
 background_color = '#f2f2f2'  # Light gray background
@@ -43,7 +45,7 @@ app.layout = html.Div(style={'background-color': background_color, 'padding': '2
     html.Br(),
     
     html.Div([
-        html.Label('Select Status:', style={'font-weight': 'bold'}),
+        html.Label('Select Status:', style={'font-weight': 'bold', 'margin-bottom': '10px'}),
         dcc.Dropdown(
             id='status-dropdown',
             options=[
@@ -67,7 +69,7 @@ app.layout = html.Div(style={'background-color': background_color, 'padding': '2
     html.Br(),
     
     html.Div([
-        html.Label('Select Methodology:', style={'font-weight': 'bold'}),
+        html.Label('Select Methodology:', style={'font-weight': 'bold', 'margin-bottom': '10px'}),
         dcc.Dropdown(
             id='method-dropdown',
             options=[
@@ -77,7 +79,33 @@ app.layout = html.Div(style={'background-color': background_color, 'padding': '2
             value='Single',
             style={'width': '100%'}
         )], style={'width': '50%', 'margin': '0 auto', 'margin-bottom': '20px'}),
-    dcc.Graph(id='cost-reduction-plot', style={'width': '100%', 'height': '80vh', 'margin': '0 auto'})
+    dcc.Graph(id='cost-reduction-plot', style={'width': '100%', 'height': '80vh', 'margin': '0 auto'}),
+    html.Br(),
+    html.Br(),
+    html.Div([
+    html.Span("Enter the capacity factors for each source:", style={'font-weight': 'bold', "display": 'block', 'text-align': 'center', 'margin-bottom': '10px'}),
+    html.Div([
+        html.Label("Offshore Wind LCOE:", style={'margin-right': '10px','margin-left': '10px'}),
+        dcc.Input(id='offshore_wind_lcoe', type='number', value=0.23, step=0.01, min=0.1, max=1),
+        html.Label("Solar LCOE:", style={'margin-right': '10px','margin-left': '10px'}),
+        dcc.Input(id='solar_lcoe', type='number', value=0.12, step=0.1, min=0.01, max=1),
+        html.Label("Onshore Wind LCOE:", style={'margin-right': '10px','margin-left': '10px'}),
+        dcc.Input(id='onshore_wind_lcoe', type='number', value=0.23, step=0.01, min=0.1, max=1),
+    ], style={'width': '50%', 'margin': '0 auto', 'margin-bottom': '10px', "display": 'block', "text-align": 'center'}),
+    html.Br(),
+
+    html.Div([
+        html.Label("Electrolyzer Type:", style={'font-weight': 'bold', 'margin-bottom': '10px'}),
+        dcc.Dropdown(
+            id='electrolyzer_type',
+            options=[
+                {'label': 'Alkaline', 'value': 'alk'},
+                {'label': 'PEM', 'value': 'pem'}
+            ],
+            value='alk', style={'width': '100%'}
+        ),
+    ], style={'width': '50%', 'margin': '0 auto', 'margin-bottom': '20px'}),
+    dcc.Graph(id='lcoh_graph')])
 
 ])
 
@@ -95,6 +123,20 @@ def update_choropleth(status):
 )
 def update_cost_reduction_plot(selected_method):
     return plot_cost_reduction(selected_method)
+
+@app.callback(
+    Output('lcoh_graph', 'figure'),
+    [
+        Input('offshore_wind_lcoe', 'value'),
+        Input('solar_lcoe', 'value'),
+        Input('onshore_wind_lcoe', 'value'),
+        Input('electrolyzer_type', 'value')
+    ]
+)
+def update_graph(offshore_wind_lcoe, solar_lcoe, onshore_wind_lcoe, electrolyzer_type):
+    global cap_factor_sources
+    cap_factor_sources = capacity_factor(offshore_wind_lcoe, solar_lcoe, onshore_wind_lcoe)
+    return global_lcoh(electrolyzer_type)
 
 # Run the app if this script is the main module
 if __name__ == '__main__':
